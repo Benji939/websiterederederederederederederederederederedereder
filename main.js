d2c5d6308c68717fd124a9d4196f18bdf05f9db0
Benji939 <76928577+Benji939@users.noreply.github.com>
2021-08-13 17:01:29 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 
-prediction_1="";
-prediction_2="";
+prediction="";
 Webcam.set({
     width:350,
     height:300,
@@ -18,4 +17,42 @@ console.log(ml5.version);
 classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/YK3rffDDx/model.json',modelLoaded);
 function modelLoaded(){
     console.log("model loaded");
+}
+function speak(){
+    var synth= window.speechSynthesis;
+    speakdata1="The prediction is"+prediction;
+    var utter=new SpeechSynthesisUtterance(speakdata1); 
+    synth.speak(utter);
+}
+function check(){
+    img= document.getElementById("captured_image");
+    classifier.classify(img, getresult);
+}
+function getresult(error, results){
+if(error){
+    console.log(error);
+}else{
+    console.log(results);
+    document.getElementById("result_gesture_name").innerHTML+results[0].label;
+    prediction=results[0].label;
+    speak();
+    if(results[0].label=="Ok"){
+        document.getElementById("update_gesture").innerHTML="&#128076;";
+}      if(results[0].label=="Good"){
+            document.getElementById("update_gesture").innerHTML="&#128077;";
+}           if(results[0].label=="Bad"){
+                document.getElementById("update_gesture").innerHTML="&#128078;";
+    }
+    if(results[0].label=="Stop"){
+        document.getElementById("update_gesture").innerHTML="&#128400;";
+}      if(results[0].label=="Rock on"){
+            document.getElementById("update_gesture").innerHTML="&#129304;";
+}           if(results[0].label=="Hello/Goodbye"){
+                document.getElementById("update_gesture").innerHTML="&#9995;";
+}
+if(results[0].label=="Stand for what you believe in"){
+    document.getElementById("update_gesture").innerHTML="&#9994;";
+}
+
+}
 }
